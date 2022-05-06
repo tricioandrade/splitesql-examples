601040eaff531ac26ac9ce25dfe7f36f95277192
Patrício Andrade <tricioandrade@outlook.com>
2022-05-07 00:53:52 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,32 @@
+<?php ?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="node_modules/@fortawesome/fontawesome-free/css/fontawesome.css">
+    <link rel="stylesheet" href="node_modules/bootstrap5/src/css/bootstrap.min.css">
     <title>Document</title>
 </head>
 <body>
-    
+    <div class="container">
+        <div class="row col-6 m-auto">
+            <form action="" method="post" class="form-row g-3">
+                <div class="col-12">
+                    <label for="exampleDataList" class="form-label">Datalist example</label>
+                    <input class="form-control" list="datalistOptions" id="exampleDataList" placeholder="Type to search...">
+                    <datalist id="datalistOptions">
+                        <option value="San Francisco">
+                        <option value="New York">
+                        <option value="Seattle">
+                        <option value="Los Angeles">
+                        <option value="Chicago">
+                    </datalist>
+                </div>
+            </form>
+        </div>
+    </div>
+    <script src="node_modules/bootstrap5/src/js/bootstrap.bundle.min.js"></script>
+    <script src="node_modules/@fortawesome/fontawesome-free/js/fontawesome.js"></script>
 </body>
 </html>

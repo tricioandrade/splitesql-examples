056d74bf514865bdb4908cafcf89d3674401e23e
Patrício Andrade <tricioandrade@outlook.com>
2022-05-12 23:33:15 +0100

    update

--- index.php
+++ index.php
@@ -1,60 +1,52 @@
-<?php ?>
+<?php 
+
+if(isset($_POST['btn-submit'])):
+
+
+
+
+
+endif;
+
+
+?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="node_modules/@fortawesome/fontawesome-free/css/fontawesome.css">
+    <link rel="stylesheet" href="node_modules/fontawesome-free/css/all.css">
     <link rel="stylesheet" href="node_modules/bootstrap5/src/css/bootstrap.min.css">
-    <title>Document</title>
+    <title>splitesql - Example usage</title>
 </head>
 <body>
     <div class="container">
         <div class="row col-6 m-auto">
-            <form class="row g-3">
+            <form action="" method="post" class="row g-3">
                 <div class="col-md-6">
-                    <label for="inputEmail4" class="form-label">Email</label>
+                    <label for="inputUserName" class="form-label"><i class="fa fa-user"></i>&nbsp;Name</label>
+                    <input type="text" class="form-control" id="inputUserName">
+                </div>
+                <div class="col-6">
+                    <label for="inputEmail4" class="form-label"><i class="fa fa-envelope"></i>&nbsp;Email</label>
                     <input type="email" class="form-control" id="inputEmail4">
                 </div>
-                <div class="col-md-6">
-                    <label for="inputPassword4" class="form-label">Password</label>
+                <div class="col-12">
+                    <label for="inputPassword4" class="form-label"><i class="fa fa-lock"></i>&nbsp;Password</label>
                     <input type="password" class="form-control" id="inputPassword4">
                 </div>
                 <div class="col-12">
-                    <label for="inputAddress" class="form-label">Address</label>
+                    <label for="inputAddress" class="form-label"><i class="fa fa-map"></i>&nbsp;Address</label>
                     <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
                 </div>
                 <div class="col-12">
-                    <label for="inputAddress2" class="form-label">Address 2</label>
-                    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
-                </div>
-                <div class="col-md-6">
-                    <label for="inputCity" class="form-label">City</label>
-                    <input type="text" class="form-control" id="inputCity">
-                </div>
-                <div class="col-md-4">
-                    <label for="inputState" class="form-label">State</label>
-                    <select id="inputState" class="form-select">
-                    <option selected>Choose...</option>
-                    <option>...</option>
-                    </select>
-                </div>
-                <div class="col-md-2">
-                    <label for="inputZip" class="form-label">Zip</label>
-                    <input type="text" class="form-control" id="inputZip">
-                </div>
-                <div class="col-12">
-                    <div class="form-check">
-                    <input class="form-check-input" type="checkbox" id="gridCheck">
-                    <label class="form-check-label" for="gridCheck">
-                        Check me out
-                    </label>
-                    </div>
-                </div>
-                <div class="col-12">
-                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <button name="btn-submit" type="submit" class="btn btn-primary">Submit</button>
                 </div>
             </form>
+        </div>
+        <hr>
+        <div class="row col-6 m-auto">
+            
         </div>
     </div>
     <script src="node_modules/bootstrap5/src/js/bootstrap.bundle.min.js"></script>
